f0486ec2d5ce1f95f6301571bef9d7adb4ed4e7b
Irregular <449683146@qq.com>
2024-06-26 13:08:06 +0800

    Necessary Update

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,10 +1,14 @@
-# Android device tree for Xiaomi 24018RPACC (sheng)
-
-```
 #
 # Copyright (C) 2024 The Android Open Source Project
 # Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_sheng.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_sheng-user \
+    omni_sheng-userdebug \
+    omni_sheng-eng
